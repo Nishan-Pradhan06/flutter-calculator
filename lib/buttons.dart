db72bef51c17c58113793387f854196ed68ae09d
nishan <nishanpradhan500@gmail.com>
2023-11-06 20:10:56 +0545

    add ontap function to key

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -2,24 +2,28 @@ import 'package:flutter/material.dart';
 
 class CalButton extends StatelessWidget {
   const CalButton(
-      {super.key, this.color, this.textColor, required this.buttonText});
+      {super.key, this.color, this.textColor, required this.buttonText,this.buttonTapped});
 
   final color;
   final textColor;
   final String buttonText;
+  final buttonTapped;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20),
-        child: Container(
-          color: color,
-          child: Center(
-            child: Text(
-              buttonText,
-              style: TextStyle(color: textColor),
+    return GestureDetector(
+      onTap: buttonTapped,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Container(
+            color: color,
+            child: Center(
+              child: Text(
+                buttonText,
+                style: TextStyle(color: textColor),
+              ),
             ),
           ),
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -52,53 +52,88 @@ class _HomePageState extends State<HomePage> {
     'ANS',
     '='
   ];
+  var userQuestion = '';
+  var userAnswer = 'hh';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple[100],
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(),
-          ),
-          Expanded(
-            flex: 3,
-            // ignore: avoid_unnecessary_containers
-            child: Container(
-              child: GridView.builder(
-                itemCount: buttons.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 4),
-                itemBuilder: (BuildContext context, int index) {
-                  if (index == 0) {
-                    return CalButton(
-                      buttonText: buttons[index],
-                      color: Colors.green,
-                      textColor: Colors.white,
-                    );
-                  } else if (index == 1) {
-                    return CalButton(
-                      buttonText: buttons[index],
-                      color: Colors.red,
-                      textColor: Colors.white,
-                    );
-                  } else {
-                    return CalButton(
-                      buttonText: buttons[index],
-                      color: isOperator(buttons[index])
-                          ? Colors.deepPurple
-                          : Colors.deepPurple[50],
-                      textColor: isOperator(buttons[index])
-                          ? Colors.white
-                          : Colors.deepPurple,
-                    );
-                  }
-                },
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color.fromARGB(255, 196, 178, 228)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          userQuestion,
+                          style: const TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          userAnswer,
+                          style: const TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
-          ),
-        ],
+            Expanded(
+              flex: 2,
+              // ignore: avoid_unnecessary_containers
+              child: Container(
+                child: GridView.builder(
+                  itemCount: buttons.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4),
+                  itemBuilder: (BuildContext context, int index) {
+                    if (index == 0) {
+                      return CalButton(
+                        buttonText: buttons[index],
+                        color: Colors.green,
+                        textColor: Colors.white,
+                      );
+                    } else if (index == 1) {
+                      return CalButton(
+                        buttonText: buttons[index],
+                        color: Colors.red,
+                        textColor: Colors.white,
+                      );
+                    } else {
+                      return CalButton(
+                        buttonTapped: () {
+                          setState(() {
+                            userQuestion += buttons[index];
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: isOperator(buttons[index])
+                            ? Colors.deepPurple
+                            : Colors.deepPurple[50],
+                        textColor: isOperator(buttons[index])
+                            ? Colors.white
+                            : Colors.deepPurple,
+                      );
+                    }
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
